570300d16e3d90dcc24dd64c32a0a6fdfb094a0d
Nick Carothers <93961846+stropkaiguy@users.noreply.github.com>
2022-11-26 13:30:10 -0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
   facebook: {
-    clientId: "703992680953239",
-    clientSecret: "69389db36b47c0cd5039ecf93994a0a6",
+    clientId: "",
+    clientSecret: "",
   },
 };
